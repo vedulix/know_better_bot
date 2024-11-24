9dba471a1c3901c36e3b78fd5dc824b0404583f1
vedulix <us.do.to@gmail.com>
2024-11-25 00:46:57 +0300

    int tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,47 @@ from sqlalchemy.orm import sessionmaker
 from tgbot.config import load_config
 from tgbot.infrastucture.database.models.base import Base
 
+class MockBot:
+    def __init__(self):
+        self.session = None
+
+class MockMessage:
+    def __init__(self, text="Test message", from_user=None, chat=None):
+        self.text = text
+        self.from_user = from_user or types.User(
+            id=123456,
+            is_bot=False,
+            first_name="Test",
+            username="test_user",
+            language_code="ru"
+        )
+        self.chat = chat or types.Chat(id=123456, type="private")
+        self.message_id = 1
+
+class MockState:
+    def __init__(self):
+        self.storage = {}
+        self.state = None
+
+    async def get_state(self):
+        return self.state
+
+    async def set_state(self, state):
+        self.state = state
+
+    async def get_data(self):
+        return self.storage
+
+    async def update_data(self, **kwargs):
+        self.storage.update(kwargs)
+
+    async def set_data(self, data):
+        self.storage = data
+
+    async def finish(self):
+        self.state = None
+        self.storage = {}
+
 @pytest.fixture(scope="session")
 def config():
     return load_config(".env.test")
@@ -19,7 +60,6 @@ async def engine(config):
         echo=True
     )
     
-    # Create all tables
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
@@ -40,40 +80,14 @@ async def session(engine):
 @pytest.fixture
 def message_mock():
     """Create mock message object"""
-    return types.Message(
-        message_id=1,
-        from_user=types.User(
-            id=123456,
-            is_bot=False,
-            first_name="Test",
-            username="test_user",
-            language_code="ru"
-        ),
-        text="Test message",
-        chat=types.Chat(id=123456, type="private")
-    )
-
-class MockState(FSMContext):
-    def __init__(self):
-        self.storage = {}
-        self.state = None
-
-    async def get_state(self):
-        return self.state
-
-    async def set_state(self, state):
-        self.state = state
-
-    async def get_data(self):
-        return self.storage
-
-    async def update_data(self, **kwargs):
-        self.storage.update(kwargs)
-
-    async def set_data(self, data):
-        self.storage = data
+    return MockMessage()
 
 @pytest.fixture
 def state_mock():
     """Create mock state object"""
-    return MockState() 
+    return MockState()
+
+@pytest.fixture
+def bot_mock():
+    """Create mock bot object"""
+    return MockBot() 
